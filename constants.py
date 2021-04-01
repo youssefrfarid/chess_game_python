317ee4145af46aa2fefb05f7354238fc6b324d26
Youssef Ramy <youssef.rfarid@gmail.com>
2021-04-01 21:13:46 +0200

    init

---
+++ constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Game
+FPS = 60
+
+# Colors
+DARKCOL = (169, 122, 101) # Dark tile color
+LIGHTCOL = pygame.Color(241, 217, 192) # light tile color
+HIGHLIGHTCOL = pygame.Color(255, 0, 0) 
+
+# Dimensions
+WIDTH, HEIGHT = 800, 800
+RANKS, FILES = 8, 8
+SQUARE_SIZE = WIDTH // FILES
---
+++ test.py
@@ -0,0 +1,1 @@
+print('Omar Noob!!')
